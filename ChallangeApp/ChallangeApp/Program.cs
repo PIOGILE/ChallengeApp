070a83cd369e93f34c9f00f1c26fabc909dcd2c1
bzabe <133162420+bzabe@users.noreply.github.com>
2023-10-26 19:31:23 +0200

    day 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Piotr";
+//string sex = "mezczyzna";
+bool isWoman = true;
+int wiek = 15;
+//string result = name + isWoman + wiek;
+//Console.WriteLine(result);
+
+if (isWoman == true && wiek <30)
+{
+    Console.WriteLine("Kobieta ponieżej 30 lat");
+}
+if (name == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if(isWoman == false && wiek < 18)
+{
+    Console.WriteLine("Niepelnoletni Mężczyzna");
+}
